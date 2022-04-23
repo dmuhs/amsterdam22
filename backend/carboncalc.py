f8567decea4741372ff4f151eb8eb57918fe2eca
tintinweb <tintinweb@oststrom.com>
2022-04-23 21:49:49 +0200

    add polygon refactor to use chainconfig

--- backend/carboncalc.py
+++ backend/carboncalc.py
@@ -2,21 +2,26 @@ import requests
 import time
 
 
+class ChainExplorerConfig:
+    ETHERSCAN = {"apiKey": "RVXR4IXM4K7TKUI2H7XQBGHZDDBP393KFP", "baseUri": "etherscan.io"} # dom
+    POLYGONSCAN = {"apiKey": "V99R51EYZRATHEK6QK3T3ACPXHFCBMFCWF", "baseUri": "api.polygonscan.com"} # rmfblqsrfthfxssbrk - password: rmfblqsrfthfxssbrk@bvhrk.com
+
 
 class CarbonCalculator(object):
     
     KG_CO2_PER_GAS = 0.0001809589427
-    ENDPOINT = "https://api.etherscan.io/api?module=account&action=txlist&address={address}&startblock={startblock}&endblock={endblock}&page={page}&offset={offset}&sort={sort}&apikey={apikey}"
+    ENDPOINT = "https://{baseUri}/api?module=account&action=txlist&address={address}&startblock={startblock}&endblock={endblock}&page={page}&offset={offset}&sort={sort}&apikey={apikey}"
 
-    def __init__(self, apiKey) -> None:
-        self.apiKey = apiKey
+    def __init__(self, chainConfig) -> None:
+        self.apiKey = chainConfig["apiKey"]
+        self.baseUri = chainConfig["baseUri"]
         self.session = requests.Session()
         
     def getTotalGasFromAllContractTransactions(self, address, txFilter):
         totalGasUsed = 0
-        results = 0
         for page in range(1, 10):
             url = self.ENDPOINT.format(
+                baseUri=self.baseUri,
                 address=address,
                 startblock=0,
                 endblock='latest',
@@ -29,12 +34,11 @@ class CarbonCalculator(object):
             resp = requests.get(url)
             data = resp.json()
             if data["status"] != "1" or data["message"] != "OK":
-                #raise Exception("Bad things happened", data)
+                if totalGasUsed == 0 and page <= 1: # first page, no gas
+                    raise Exception("Bad things happened", data) # assume error
                 return totalGasUsed
             if data["result"] == []:
                 return totalGasUsed 
-            results += len(data["result"])
-            print(f"result now at {results}")
             for tx in data["result"]:
                 if txFilter and not txFilter(tx):
                     continue
@@ -49,7 +53,7 @@ class CarbonCalculator(object):
 
 
 def test():
-    cc = CarbonCalculator("RVXR4IXM4K7TKUI2H7XQBGHZDDBP393KFP")
+    cc = CarbonCalculator(ChainExplorerConfig.POLYGONSCAN)
     print(cc.getCarbonFootprintForContractAddress("0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48", lambda tx: int(tx["timeStamp"])>=int(time.time()) - 60*60*24 )) # carbon footprint for all TX
 
 if __name__ == "__main__":
