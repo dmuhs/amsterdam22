ab1020a54a5e4ebd7566ed142209b43794c4811d
Dominik Muhs <dmuhs@protonmail.ch>
2022-04-23 22:39:10 +0000

    refactor server setup

--- backend/server.py
+++ backend/server.py
@@ -3,13 +3,13 @@ import falcon
 import resources
 
 
-app = falcon.App(
-    middleware=falcon.CORSMiddleware(allow_origins="*", allow_credentials="*")
-)
+cors = falcon.CORSMiddleware(allow_origins="*", allow_credentials="*")
+app = falcon.App(middleware=cors)
+
+app.add_route("/", resources.RedirectResource())
+app.add_route("/estimate", resources.EstimationResource())
 app.add_route("/success", resources.SuccessCallbackResource())
 app.add_route("/failure", resources.FailureCallbackResource())
-app.add_route("/estimate", resources.EstimationResource())
-app.add_route("/", resources.RedirectResource())
 
 if __name__ == "__main__":
     with make_server("", 8000, app) as httpd:
