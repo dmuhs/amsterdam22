8bdeb3ee1d0429f3f24534c95207c1d4e69cf3b4
tintinweb <tintinweb@oststrom.com>
2022-04-23 22:31:01 +0200

    add polygonscan

--- backend/carboncalc.py
+++ backend/carboncalc.py
@@ -2,16 +2,24 @@ import requests
 import time
 
 
+class ChainExplorerConfig:
+    ETHERSCAN = {"apiKey": "RVXR4IXM4K7TKUI2H7XQBGHZDDBP393KFP", "baseUri": "api.etherscan.io"} # dom
+    POLYGONSCAN = {"apiKey": "V99R51EYZRATHEK6QK3T3ACPXHFCBMFCWF", "baseUri": "api.polygonscan.com"} # rmfblqsrfthfxssbrk - password: rmfblqsrfthfxssbrk@bvhrk.com (throwaway)
+
+
+
 class CarbonCalculator:
 
     G_CO2_PER_GAS = 0.1809589427
-    ENDPOINT = "https://api.etherscan.io/api?module=account&action=txlist&address={address}&startblock={startblock}&endblock={endblock}&page={page}&offset={offset}&sort={sort}&apikey={apikey}"
+    ENDPOINT = "https://{baseUri}/api?module=account&action=txlist&address={address}&startblock={startblock}&endblock={endblock}&page={page}&offset={offset}&sort={sort}&apikey={apikey}"
 
-    def __init__(self, api_key) -> None:
-        self.api_key = api_key
+    def __init__(self, chainConfig) -> None:
+        self.api_key = chainConfig["apiKey"]
+        self.baseUri = chainConfig["baseUri"]
 
     def getTotalGasFromAllContractTransactions(self, address, txFilter):
         url = self.ENDPOINT.format(
+            baseUri=self.baseUri,
             address=address,
             startblock=0,
             endblock="latest",
@@ -49,7 +57,7 @@ class CarbonCalculator:
 
 
 if __name__ == "__main__":
-    cc = CarbonCalculator("RVXR4IXM4K7TKUI2H7XQBGHZDDBP393KFP")
+    cc = CarbonCalculator(ChainExplorerConfig.ETHERSCAN)
     print(
         cc.getCarbonFootprintForContractAddress(
             "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48",
