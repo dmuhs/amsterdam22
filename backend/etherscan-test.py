a652fe605fe5c9e658ec7a22e9d620b6100989d0
Dominik Muhs <dmuhs@protonmail.ch>
2022-04-23 13:03:38 +0000

    init

---
+++ backend/etherscan-test.py
@@ -0,0 +1,30 @@
+import requests
+import sys
+
+
+API_KEY = "RVXR4IXM4K7TKUI2H7XQBGHZDDBP393KFP"
+ENDPOINT = "https://api.etherscan.io/api?module=account&action=txlist&address={address}&startblock={startblock}&endblock={endblock}&page={page}&offset={offset}&sort={sort}&apikey={apikey}"
+
+results = 0
+
+for page in range(1, 10):
+    resp = requests.get(ENDPOINT.format(
+        address="0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48",
+        startblock=0,
+        endblock=999999999,
+        page=page,
+        offset=10000,
+        sort="asc",
+        apikey=API_KEY
+    ))
+
+    data = resp.json()
+    if data["status"] != "1" or data["message"] != "OK":
+        print("Bad things happened")
+        print(data)
+        sys.exit()
+    if data["result"] == []:
+        print("Done")
+        sys.exit()
+    results += len(data["result"])
+    print(f"result now at {results}")
---
+++ backend/server.py
@@ -0,0 +1,59 @@
+# examples/things.py
+
+# Let's get this party started!
+from wsgiref.simple_server import make_server
+from falcon import Request, Response
+import falcon
+
+PATCH_API_KEY = "key_test_406d0d4c03d8da034afb695155318569"
+BASE_URL = "https://api.patch.io"
+
+
+
+
+# TODO: build erc721 contract, generate local privkey, set pubkey as owner
+
+class SuccessCallbackResource:
+    def on_get(self, req: Request, resp: Response):
+        # TODO: Whitelist origin 80.113.211.254
+
+        doc = req.stream.read(req.content_length or 0)
+        print(f"Data: {doc}")
+        print(f"Headers: {req.headers}")
+        print(f"Params: {req.params}")
+        # TODO: extract param "metadata[address]" and store
+
+        # TODO: check patch api whether order paid
+
+        # TODO: assemble tx to mint nft
+
+        # TODO: submit to infura
+
+        # TODO: redirect back to frontend
+        resp.status = falcon.HTTP_200
+        resp.content_type = falcon.MEDIA_TEXT
+        resp.text = "success"
+
+
+class FailureCallbackResource:
+    def on_get(self, req: Request, resp: Response):
+        doc = req.stream.read(req.content_length or 0)
+        print(f"Data: {doc}")
+        print(f"Headers: {req.headers}")
+        print(f"Params: {req.params}")
+        resp.status = falcon.HTTP_200
+        resp.content_type = falcon.MEDIA_TEXT
+        resp.text = "failure"
+
+
+app = falcon.App()
+app.add_route('/success', SuccessCallbackResource())
+app.add_route('/failure', FailureCallbackResource())
+
+
+if __name__ == '__main__':
+    with make_server('', 8000, app) as httpd:
+        print('Serving on port 8000...')
+
+        # Serve until process is killed
+        httpd.serve_forever()
