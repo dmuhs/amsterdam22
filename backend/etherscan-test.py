ed32dd35f2b8e8f968e8032f316c151e30d6e618
Dominik Muhs <dmuhs@protonmail.ch>
2022-04-23 16:18:53 +0000

    Add tx logic to server

--- backend/etherscan-test.py
+++ backend/etherscan-test.py
@@ -8,15 +8,17 @@ ENDPOINT = "https://api.etherscan.io/api?module=account&action=txlist&address={a
 results = 0
 
 for page in range(1, 10):
-    resp = requests.get(ENDPOINT.format(
-        address="0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48",
-        startblock=0,
-        endblock=999999999,
-        page=page,
-        offset=10000,
-        sort="asc",
-        apikey=API_KEY
-    ))
+    resp = requests.get(
+        ENDPOINT.format(
+            address="0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48",
+            startblock=0,
+            endblock=999999999,
+            page=page,
+            offset=10000,
+            sort="asc",
+            apikey=API_KEY,
+        )
+    )
 
     data = resp.json()
     if data["status"] != "1" or data["message"] != "OK":
--- backend/server.py
+++ backend/server.py
@@ -9,11 +9,14 @@ from web3py import Web3
 PATCH_API_KEY = "key_test_406d0d4c03d8da034afb695155318569"
 BASE_URL = "https://api.patch.io"
 NFT_ADDR = "0x63C93aC0D99EB331b3c0711b5F06721745b73eec"
-PUBLIC_KEY = "0x2ca0649D6F544bFF02f3917E2419693d094166D2"
-PRIVATE_KEY = "49e81553780e86df5e537ae647e63126bf8384204284b3491c20825a72129771"
-ABI = ""
-INFURA_URL = ""
+PUBLIC_KEY = "0x3ded79F5141f5c0aF23c6c7907A6E279160542FB"
+PRIVATE_KEY = "0x59f3622b1ecd1a25f042d893fa6d101194c6722840f55771c131731f1a204e65"
+ABI = '[{"inputs":[],"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"owner","type":"address"},{"indexed":true,"internalType":"address","name":"approved","type":"address"},{"indexed":true,"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"Approval","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"owner","type":"address"},{"indexed":true,"internalType":"address","name":"operator","type":"address"},{"indexed":false,"internalType":"bool","name":"approved","type":"bool"}],"name":"ApprovalForAll","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"previousOwner","type":"address"},{"indexed":true,"internalType":"address","name":"newOwner","type":"address"}],"name":"OwnershipTransferred","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"from","type":"address"},{"indexed":true,"internalType":"address","name":"to","type":"address"},{"indexed":true,"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"Transfer","type":"event"},{"inputs":[{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"approve","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"owner","type":"address"}],"name":"balanceOf","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"getApproved","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"owner","type":"address"},{"internalType":"address","name":"operator","type":"address"}],"name":"isApprovedForAll","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"name","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"owner","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"ownerOf","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"renounceOwnership","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"to","type":"address"}],"name":"safeMint","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"from","type":"address"},{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"safeTransferFrom","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"from","type":"address"},{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"tokenId","type":"uint256"},{"internalType":"bytes","name":"_data","type":"bytes"}],"name":"safeTransferFrom","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"operator","type":"address"},{"internalType":"bool","name":"approved","type":"bool"}],"name":"setApprovalForAll","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"bytes4","name":"interfaceId","type":"bytes4"}],"name":"supportsInterface","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"symbol","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"tokenAmount","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"tokenURI","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"from","type":"address"},{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"transferFrom","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"newOwner","type":"address"}],"name":"transferOwnership","outputs":[],"stateMutability":"nonpayable","type":"function"}]'
+INFURA_URL = "https://polygon-mumbai.infura.io/v3/5e3d186095d04216ba93cd1c52a6ad61"
+
+
 # TODO: build erc721 contract, generate local privkey, set pubkey as owner
+
 
 class SuccessCallbackResource:
     def on_get(self, req: Request, resp: Response):
@@ -34,10 +37,23 @@ class SuccessCallbackResource:
 
         # TODO: check patch api whether order paid
 
-        # TODO: mint nft to target addr
+        # prepare minting tx
         w3 = Web3(Web3.HTTPProvider(INFURA_URL))
+        nonce = w3.eth.get_transaction_count(target_addr)
         contract_instance = w3.eth.contract(address=NFT_ADDR, abi=ABI)
-        contract_instance.functions.safeMint(target_addr).transact()
+        tx = contract_instance.functions.safeMint(target_addr).buildTransaction(
+            {
+                "chainId": 137,
+                "gas": 70000,
+                "maxFeePerGas": w3.toWei("2", "gwei"),
+                "maxPriorityFeePerGas": w3.toWei("1", "gwei"),
+                "nonce": nonce,
+            }
+        )
+
+        # send mint tx
+        signed_tx = w3.eth.account.sign_transaction(tx, private_key=PRIVATE_KEY)
+        w3.eth.send_raw_transaction(signed_tx.rawTransaction)
 
         # TODO: redirect back to frontend
         resp.status = falcon.HTTP_200
@@ -57,13 +73,13 @@ class FailureCallbackResource:
 
 
 app = falcon.App()
-app.add_route('/success', SuccessCallbackResource())
-app.add_route('/failure', FailureCallbackResource())
+app.add_route("/success", SuccessCallbackResource())
+app.add_route("/failure", FailureCallbackResource())
 
 
-if __name__ == '__main__':
-    with make_server('', 8000, app) as httpd:
-        print('Serving on port 8000...')
+if __name__ == "__main__":
+    with make_server("", 8000, app) as httpd:
+        print("Serving on port 8000...")
 
         # Serve until process is killed
         httpd.serve_forever()
